b747161e27b1246d8744411c937af675c52f6260
guoliang <tswanggl@gmail.com>
2021-10-11 19:06:06 +0800

    change the bool.cpp

--- week02/examples/bool.cpp
+++ week02/examples/bool.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <typeinfo>
+
 using namespace std;
 
 int main()
@@ -6,9 +8,9 @@ int main()
     bool b1 = true;
     int i = b1;
     bool b2 = -256;
-    cout << "i=" << i << endl;
-    cout << "b1=" << b1 << endl;
-    cout << "b2=" << b2 << endl;
+    cout << "i = " << i << " the type of the i is " << typeid(i).name() << endl;
+    cout << "b1 = " << b1 << " the type of the b1 is " << typeid(b1).name() << endl;
+    cout << "b2 = " << b2 << " the type of the b2 is " << typeid(b2).name() << endl;
     cout << true << endl;
     return 0;
 }
